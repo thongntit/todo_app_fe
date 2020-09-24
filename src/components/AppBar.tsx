abe8cdc98188ef9729d2f18402306094bd0ed8e5
Thong Nguyen <nanolove95@gmail.com>
2020-09-24 23:52:52 +0700

    :zap: Separate appbar & sidebar

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -10,7 +10,8 @@ import IconButton from '@material-ui/core/IconButton';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import MenuIcon from '@material-ui/icons/Menu';
-import React, { useState } from 'react';
+import React, { useState, useContext, Dispatch } from 'react';
+import AuthContext from 'contexts/auth';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -22,17 +23,30 @@ const useStyles = makeStyles((theme) => ({
   title: {
     flexGrow: 1,
   },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
 }));
-
-type AppHeaderProps = {
-  user: any;
-  logOut: any;
-};
-const AppHeader: React.FC<AppHeaderProps> = ({ user, logOut }) => {
+interface IUserInfo {
+  ggInfo?: {
+    name: string;
+    picture: string;
+  };
+}
+const AppHeader: React.FC<{ openSideBar: Dispatch<any> }> = ({
+  openSideBar,
+}) => {
+  const { userInfo, logOut }: { userInfo: IUserInfo; logOut: any } = useContext(
+    AuthContext
+  );
   const classes = useStyles();
   const [open, setOpen] = useState(false);
   const [anchorEl, setAnchorEl] = React.useState<HTMLElement | null>(null);
-  const handleClick = () => {
+  const onClickUserMenu = () => {
     const element = document.getElementById('avatar-picture');
     setAnchorEl(element);
     setOpen(true);
@@ -42,19 +56,19 @@ const AppHeader: React.FC<AppHeaderProps> = ({ user, logOut }) => {
   };
   return (
     <div>
-      <AppBar position="static">
+      <AppBar position="relative" className={classes.appBar}>
         <Toolbar>
-          <IconButton>
+          <IconButton onClick={openSideBar}>
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
             Việc phải làm
           </Typography>
-          <IconButton color="inherit" onClick={handleClick}>
+          <IconButton color="inherit" onClick={onClickUserMenu}>
             <Avatar
               id="avatar-picture"
               alt="Profile"
-              src={user?.ggInfo?.picture}
+              src={userInfo?.ggInfo?.picture}
             />
           </IconButton>
           <Popover
@@ -70,9 +84,7 @@ const AppHeader: React.FC<AppHeaderProps> = ({ user, logOut }) => {
             open={open}
             onClose={handleClose}
           >
-            <MenuItem>{user?.ggInfo?.name}</MenuItem>
-            <Divider />
-            <MenuItem>{'Phiên bản 0.1.0'}</MenuItem>
+            <MenuItem>{userInfo?.ggInfo?.name}</MenuItem>
             <Divider />
             <MenuItem onClick={logOut}>Thoát</MenuItem>
           </Popover>
