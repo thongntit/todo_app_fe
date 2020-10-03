09bc25c2b52c7292bc329bf806ddfcf9e703397b
Thong Nguyen <nanolove95@gmail.com>
2020-10-03 22:44:24 +0700

    :bug: only getTodos if login

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,11 +1,15 @@
+import AuthContext from 'contexts/auth';
 import React, { useContext, useEffect } from 'react';
 import TodoList from '../../components/TodoList';
 import TodosContext from '../../contexts/todos';
 
 const Home = () => {
   const todos = useContext(TodosContext);
+  const { isLogin } = useContext(AuthContext)
   useEffect(() => {
-    todos.getTodos();
+    if (isLogin) {
+      todos.getTodos();
+    }
     // eslint-disable-next-line
   }, []);
   return (
